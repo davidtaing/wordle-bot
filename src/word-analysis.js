c22348b63639f731fe02ce78e864886509a106a7
David Taing <adavidtaing@gmail.com>
2022-02-20 11:12:00 +1100

    refactor: remove unused map key/value init

--- src/word-analysis.js
+++ src/word-analysis.js
@@ -18,20 +18,15 @@ function getLetterFreq() {
 }
 
 function incrementLetterFreq(map, char) {
-  if (map.get(char) === undefined) {
-    map.set(char, 0);
-  } else {
-    map.set(char, map.get(char) + 1);
-  }
+  map.set(char, map.get(char) + 1);
 }
 
 function initLetterMap() {
   const letterMap = new Map();
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
 
-  for (const letter of alphabet) {
+  for (const letter of alphabet)
     letterMap.set(letter, 0);
-  }
 
   return letterMap;
 }
